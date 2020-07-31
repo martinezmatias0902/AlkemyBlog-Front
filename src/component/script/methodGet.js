a81b58c9e0d9174c949573837ae2e4ed7d3957c1
martinezmatias0902 <martinez.matias0902@gmail.com>
2020-07-31 10:32:22 -0300

    get by id

--- src/component/script/methodGet.js
+++ src/component/script/methodGet.js
@@ -13,7 +13,79 @@ getAllPosts.addEventListener('click', (event) => {
         url: 'http://127.0.0.1:8080/api/blog'
     })
     .then(res => {
-        console.log(res.data);
+        for (const post of res.data) {
+            //ASIDE section
+            const crearLi = document.createElement('li');
+            crearLi.textContent = `${post.id} - ${post.timestamp}`;
+            listOfLastPosts.appendChild(crearLi); 
+            
+            //MAIN section
+            //General container
+            const contenedor = document.createElement('div');
+            contenedor.setAttribute('class', 'post');
+            postSection.appendChild(contenedor);
+
+            //Manipular ID - Titulo
+            const contenedorIdyTitulo = document.createElement('div');
+            const h3 = document.createElement('h3');
+            const p = document.createElement('p');
+            p.setAttribute('id', 'idDeUnPost')
+            contenedorIdyTitulo.setAttribute('class', 'postIdyFecha');
+
+            h3.textContent = `${post.title}`;
+            p.textContent = `${post.id}`;
+
+            contenedor.appendChild(contenedorIdyTitulo);
+            contenedorIdyTitulo.appendChild(h3);
+            contenedorIdyTitulo.appendChild(p);
+            
+            //Manipular Texto del post
+            const textContainer = document.createElement('div');
+            textContainer.setAttribute('class', 'postText');
+            textContainer.textContent = `${post.text}`;
+            contenedor.appendChild(textContainer)
+
+            //Fecha
+            const fechaContainer = document.createElement('span');
+            fechaContainer.textContent = `Fecha: ${post.timestamp}`
+            contenedor.appendChild(fechaContainer);
+
+            //Botones EDIT - DELETE
+            const contenedorBotones = document.createElement('div');
+            contenedorBotones.setAttribute('class', 'actOnPost');
+            const toedit = document.createElement('a');
+            const todelete = document.createElement('a');
+            toedit.setAttribute('id', 'editoTexto');
+            todelete.setAttribute('class', 'eliminoPost');
+            toedit.textContent = `EDIT`;
+            todelete.textContent = `DELETE`;
+            contenedor.appendChild(contenedorBotones);
+            contenedorBotones.appendChild(toedit);
+            contenedorBotones.appendChild(todelete);
+
+        }
+    })
+    .catch(error => {
+        // handle error
+        console.log(error);
+      })
+});
+
+//Get Posts by ID
+
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const datos = new FormData(formulario);
+
+    axios({
+        method: 'GET',
+        url: `http://127.0.0.1:8080/api/blog/${datos.get('idBlog')}`
+    })
+    .then(res => {
+        //handle response
         for (const post of res.data) {
             //ASIDE section
             const crearLi = document.createElement('li');
